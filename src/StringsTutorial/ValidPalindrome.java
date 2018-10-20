98736a816b42817bbea816922585aaa8e0ed0b54
Peter Njenga <njengasaruni@gmail.com>
2018-10-20 23:25:05 +0300

    Accepted solution for isPalindrome() using out-middle approach

--- src/StringsTutorial/ValidPalindrome.java
+++ src/StringsTutorial/ValidPalindrome.java
@@ -13,14 +13,34 @@
 
         Thought process:
 
-        Middle out -> Move with left, right pointers, non-alpha would be a problem*/
+        Out to middle, two finger algorithm.*/
+
 package StringsTutorial;
 
 public class ValidPalindrome{
-    public boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
 
+        while(left <= right){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))) {
+                right--;
+                left++;
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
     public static void main(String[] args){
-
+        System.out.print(isPalindrome("I am ai"));
     }
 }

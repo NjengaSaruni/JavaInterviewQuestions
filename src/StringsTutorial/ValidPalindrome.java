74add46537aa8a4d40ac3b2822326dd0146c35dd
Peter Njenga <njengasaruni@gmail.com>
2018-10-20 21:47:15 +0300

    Is Palindrome

---
+++ src/StringsTutorial/ValidPalindrome.java
@@ -0,0 +1,26 @@
+/*Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+        Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+        Example 1:
+
+        Input: "A man, a plan, a canal: Panama"
+        Output: true
+        Example 2:
+
+        Input: "race a car"
+        Output: false
+
+        Thought process:
+
+        Middle out -> Move with left, right pointers, non-alpha would be a problem*/
+package StringsTutorial;
+
+public class ValidPalindrome{
+    public boolean isPalindrome(String s) {
+
+    }
+    public static void main(String[] args){
+
+    }
+}

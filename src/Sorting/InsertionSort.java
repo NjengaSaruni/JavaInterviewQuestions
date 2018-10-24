73690a42cd0b09244abb490469d475de92a47a25
Peter Njenga <njengasaruni@gmail.com>
2018-10-25 00:31:32 +0300

    Half working insertion sort

--- src/Sorting/InsertionSort.java
+++ src/Sorting/InsertionSort.java
@@ -1,29 +1,42 @@
 package Sorting;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class InsertionSort {
 
     public static void sort(int[] array){
-        int[] answer = new int[array.length];
-        for(int i = 0; i < array.length - 1; i++){
-            int minIndex = i;
-            for(int j = i + 1; j < array.length; j++){
-                if(array[j] < array[minIndex]){
-                    minIndex = j;
+        System.out.println(Arrays.toString(array));
+        for(int i = 1; i < array.length; i++){
+            int toInsert = array[i];
+            System.out.printf("Iteration %d to insert %d\n", i, toInsert);
+            int j = i - 1;
+            while(j >= 0 && array[j] > toInsert){
+                System.out.printf("Shift %d from %d to %d -> \t\t", array[j], j , j+ 1);
+                array[j + 1] = array[j];
+                System.out.printf("%s\t\t",Arrays.toString(array));
+                System.out.printf("Carry value is %d\n", toInsert);
+
+                if(j > 0){
+                    j--;
+                }
+                else{
+                    break;
                 }
             }
-            int hold = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = hold;
+            if( j < i - 1){
+                System.out.printf("Finally insert %d in hole %d\t\t", toInsert, j);
+                array[j] = toInsert;
+            }else{
+                System.out.printf("No insertion necessary\t\t\t");
+            }
 
-            System.out.println(Arrays.toString(array));
+            System.out.printf("%s\n\n", Arrays.toString(array));
+
         }
     }
 
     public static void main(String[] args){
-        int[] array = new int[]{1,5,7,3,66,3,2,7,88,5};
+        int[] array = new int[]{17,15,7,3,66,453,2,7,88,5};
         sort(array);
 
         System.out.println(Arrays.toString(array));

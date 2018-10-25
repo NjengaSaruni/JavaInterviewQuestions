858e87957eae5c5f1b2e62bd850b0ca0003b3324
Peter Njenga <njengasaruni@gmail.com>
2018-10-25 16:16:26 +0300

    Merge sort that I figured out myself just like that cause I can

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,72 @@
+/*Thought process
+*
+* [4,6,2,66,2,1,66,76]
+*
+* [4,6,2,66] --- [2,1,6,76]
+*
+* [4,6] [2,66] --- [2, 1] [6,76]
+*
+* [4] [6]  --- [2, 66] --- [2] [1] --- [6] [76]
+*
+* [4] --- [6] --- [2] --- [66] --- [2] --- [1] --- [6] --- [76]
+*
+* []
+**/
+package Sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    private static int[] sort(int[] array) {
+        if (array.length == 1) {
+            return array;
+        }
+        int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+        int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
+        int[] answer = new int[array.length];
+
+        System.out.printf("Left:\t\t %s\n", Arrays.toString(left));
+        System.out.printf("Right:\t\t %s\n", Arrays.toString(right));
+
+        int l = 0;
+        int r = 0;
+        int i = 0;
+        left = sort(left);
+        right = sort(right);
+        while (l < left.length && r < right.length) {
+            if (left[l] < right[r]) {
+                answer[i] = left[l];
+                l++;
+            } else {
+                answer[i] = right[r];
+                r++;
+            }
+            i++;
+        }
+        while(l < left.length){
+            answer[i] = left[l];
+            l++;
+            i++;
+        }
+        while(r < right.length){
+            answer[i] = right[r];
+            r++;
+            i++;
+        }
+
+        System.out.printf("Current answer is %s\n\n", Arrays.toString(answer));
+
+        return answer;
+
+
+    }
+    public static void main(String[] args){
+        int[] array = new int[]{1, 4,6,2,66,2,10909,66,80,76};
+        sort(array);
+        System.out.println(Arrays.toString(sort(array)));
+    }
+
+    MergeSort(){
+        System.out.println("Called constructor");
+    }
+}

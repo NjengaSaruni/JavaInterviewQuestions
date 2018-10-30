b4b496b6f9d3cf08c5b2bece41cb46835fb15c1a
Peter Njenga <njengasaruni@gmail.com>
2018-10-30 09:51:22 +0300

    Failed attempt to answer an easy question

--- src/LeetCode/Easy/Easy88MergeSortedArray.java
+++ src/LeetCode/Easy/Easy88MergeSortedArray.java
@@ -18,24 +18,16 @@ import java.util.Arrays;
 public class Easy88MergeSortedArray {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m + n - 1;
-        m--;
-        n--;
-        outer:
-        while(m >= 0 || n >= 0){
-            if(m < 0){
-                while(n >= 0) {
-                    nums1[i] = nums2[n];
-                    n--;
-                }
-                break;
-            }else if(n < 0){
-                while(m >= 0){
-                    nums1[i] = nums1[m];
-                    m--;
-                }
-                break;
+        while(i >= 0){
+            if(m - 1 < 0){
+                nums1[i] = nums1[n - 1];
+                n--;
+            }else if(n - 1 < 0){
+                nums1[i] = nums1[m - 1];
+                n--;
+                z
             }
-            if(nums1[m] >= nums2[n]){
+            if(nums1[m - 1] >= nums2[n - 1]){
                 nums1[i] = nums1[m];
                 m--;
             }else{

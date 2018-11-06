dfac0623f02c5b04f2a8e9f708c8fa6236213194
Peter Njenga <peternjengask@gmail.com>
2018-11-07 00:22:23 +0300

    Just some fumbling

--- src/CrackingTheInterview/P1PatternMatching.java
+++ src/CrackingTheInterview/P1PatternMatching.java
@@ -2,13 +2,21 @@ package CrackingTheInterview;
 
 public class P1PatternMatching {
 
-    public static int minimum(int[] array){
-        int mid = array.length / 2;
+    public static int minimum(int[] array, int left, int right){
+        if(left == right){
+            return array[left];
+        }
+        int mid = (left + right) / 2;
 
-        return array[mid];
+        if(array[left] > array[mid]){
+            return minimum(array, left + 1, mid);
+        }
+
+        return -1;
     }
 
-    public static void main(){
-        System.out.println(minimum(new int[]{8,9,1,3,5}));
+    public static void main(String[] args){
+        int[] array = new int[]{8,9,1,3,5};
+        System.out.println(minimum(array, 0, array.length - 1));
     }
 }

07dff15647c6da5d318da67260e6299e7378c993
Peter Njenga <peternjengask@gmail.com>
2018-11-07 15:49:38 +0300

    Start work on examplification using hour hand example

---
+++ src/CrackingTheInterview/P1ExamplifyHourHand.java
@@ -0,0 +1,10 @@
+package CrackingTheInterview;
+
+public class P1ExamplifyHourHand {
+    public static int angle(int hour, int minute){
+        return 0;
+    }
+    public static void main(String[] args){
+
+    }
+}

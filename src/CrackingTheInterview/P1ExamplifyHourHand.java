845791e691bbba3fdac03a9800a8749f10435a28
Peter Njenga <peternjengask@gmail.com>
2018-11-07 16:06:40 +0300

    Working code to examplification question

--- src/CrackingTheInterview/P1ExamplifyHourHand.java
+++ src/CrackingTheInterview/P1ExamplifyHourHand.java
@@ -2,9 +2,9 @@ package CrackingTheInterview;
 
 public class P1ExamplifyHourHand {
     public static int angle(int hour, int minute){
-        return 0;
+        return Math.abs((hour % 12) * 30 - minute * 6);
     }
     public static void main(String[] args){
-
+        System.out.println(angle(12, 30));
     }
 }

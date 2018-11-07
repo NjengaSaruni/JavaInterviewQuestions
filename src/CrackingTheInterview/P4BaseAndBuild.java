658f512eab402cf3f9212bc8cf0e72a40c4f217d
Peter Njenga <peternjengask@gmail.com>
2018-11-07 18:52:11 +0300

    Remove the not so optistic looking code for base and recurse problem

--- src/CrackingTheInterview/P4BaseAndBuild.java
+++ src/CrackingTheInterview/P4BaseAndBuild.java
@@ -8,14 +8,7 @@ public class P4BaseAndBuild {
         List<String> ls = new ArrayList<>();
         char[] strchars = str.toCharArray();
 
-        if(strchars.length == 0){
-            return ls;
-        }
-        if(strchars.length == 1){
-            ls.add(str);
-            return ls;
-        }
-
+        return ls;
     }
 
     public static void main(String[] args){

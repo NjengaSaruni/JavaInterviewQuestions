bcf899ef912b445e8aada15c99576fcb5845d439
Peter Njenga <peternjengask@gmail.com>
2018-11-07 15:42:33 +0300

    Solution to searching for minimum element in rotated sorted array

--- src/CrackingTheInterview/P1PatternMatching.java
+++ src/CrackingTheInterview/P1PatternMatching.java
@@ -3,11 +3,23 @@ package CrackingTheInterview;
 public class P1PatternMatching {
 
     public static int minimum(int[] array, int left, int right){
-        return 0;
+        if(left == right){
+            return array[left];
+        }
+
+        int mid = (left + right + 1) / 2;
+
+        if(array[left] > array[mid]){
+            if(array[mid] < array[mid -1]) return array[mid];
+            return minimum(array, left + 1, mid - 1);
+        }else {
+            if(array[left] < array[right]) return array[left];
+            return minimum(array, mid + 1, right);
+        }
     }
 
     public static void main(String[] args){
-        int[] array = new int[]{8, 9, 12, 1, 3, 5, 7, 7};
+        int[] array = new int[]{9, 10, 1, 2, 3, 5, 5, 6, 9};
         System.out.println(minimum(array, 0, array.length - 1));
     }
 }

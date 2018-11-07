4869a9804aaad287bef66bcf38fbc8f86e3d64f4
Peter Njenga <peternjengask@gmail.com>
2018-11-07 15:08:57 +0300

    Restart it all

--- src/CrackingTheInterview/P1PatternMatching.java
+++ src/CrackingTheInterview/P1PatternMatching.java
@@ -3,32 +3,7 @@ package CrackingTheInterview;
 public class P1PatternMatching {
 
     public static int minimum(int[] array, int left, int right){
-        if(left == right){
-            return array[left];
-        }
-        if((int)Math.pow(right - left, 2) == 1){
-            if(array[right] < array[left]){
-                return array[right];
-            }else{
-                return array[left];
-            }
-        }
-        int mid = (left + right) / 2;
-
-        if((int)Math.pow(mid - left, 2) == 1){
-            if(array[mid] < array[left]){
-                return array[mid];
-            }else{
-                return array[left];
-            }
-        }
-
-
-        if(array[left] > array[mid]){
-            return minimum(array, left , mid);
-        }else{
-            return minimum(array, mid + 1, right);
-        }
+        return 0;
     }
 
     public static void main(String[] args){

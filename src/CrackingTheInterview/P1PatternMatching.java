fb4e0f88442940955f34819ab0b7d4b1d9a27042
Peter Njenga <peternjengask@gmail.com>
2018-11-07 12:42:09 +0300

    Non working solution to interview question problem

--- src/CrackingTheInterview/P1PatternMatching.java
+++ src/CrackingTheInterview/P1PatternMatching.java
@@ -6,17 +6,33 @@ public class P1PatternMatching {
         if(left == right){
             return array[left];
         }
+        if((int)Math.pow(right - left, 2) == 1){
+            if(array[right] < array[left]){
+                return array[right];
+            }else{
+                return array[left];
+            }
+        }
         int mid = (left + right) / 2;
 
-        if(array[left] > array[mid]){
-            return minimum(array, left + 1, mid);
+        if((int)Math.pow(mid - left, 2) == 1){
+            if(array[mid] < array[left]){
+                return array[mid];
+            }else{
+                return array[left];
+            }
         }
 
-        return -1;
+
+        if(array[left] > array[mid]){
+            return minimum(array, left , mid);
+        }else{
+            return minimum(array, mid + 1, right);
+        }
     }
 
     public static void main(String[] args){
-        int[] array = new int[]{8,9,1,3,5};
+        int[] array = new int[]{8, 9, 12, 1, 3, 5, 7, 7};
         System.out.println(minimum(array, 0, array.length - 1));
     }
 }

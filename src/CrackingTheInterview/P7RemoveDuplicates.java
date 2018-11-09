ed368aacf63c44bba5664964ff10b2ea491aa8bb
Peter Njenga <peternjengask@gmail.com>
2018-11-09 16:18:13 +0300

    Solution

--- src/CrackingTheInterview/P7RemoveDuplicates.java
+++ src/CrackingTheInterview/P7RemoveDuplicates.java
@@ -1,12 +1,30 @@
 package CrackingTheInterview;
 
-public class P7RemoveDuplicates {
-    public static char[] removeDuplicates(){
+import java.util.HashSet;
+import java.util.Set;
 
+public class P7RemoveDuplicates {
+    public static void removeDuplicates(char[] str){
+        if(str.length == 0){
+            return;
+        }
+        Set<Character> set = new HashSet<>();
+        int fast = 1;
+        int slow = 0;
+        set.add(str[0]);
+        while(fast < str.length){
+            if(!set.contains(str[fast])){
+                slow++;
+                str[slow] = str[fast];
+                set.add(str[slow]);
+            }
+            fast++;
+        }
     }
 
     public static void main(String[] args){
-        char[] str = new char[] {'a', 'b', 'd', 'd', 'e', 'f'};
-        System.out.println(removeDuplicates());
+        char[] str = new char[] {};
+        removeDuplicates(str);
+        System.out.println(str);
     }
 }

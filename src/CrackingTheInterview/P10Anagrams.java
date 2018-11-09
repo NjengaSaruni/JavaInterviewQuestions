a31c14106eb38350c9b667ef0b602012689967c0
Peter Njenga <peternjengask@gmail.com>
2018-11-09 22:59:41 +0300

    Solve by using the idea of a char is inherently an int

--- src/CrackingTheInterview/P10Anagrams.java
+++ src/CrackingTheInterview/P10Anagrams.java
@@ -1,4 +1,40 @@
 package CrackingTheInterview;
 
+import java.util.Arrays;
+
 public class P10Anagrams {
+
+    private static boolean areAnagrams(String a, String b){
+        char[] achars = a.toCharArray();
+        char[] bchars = b.toCharArray();
+
+        Arrays.sort(achars);
+        Arrays.sort(bchars);
+
+        return new String(achars).equals(new String(bchars));
+    }
+
+    private static boolean areAnagramsArrayASCII(String a, String b) {
+        int[] valuesA = new int[256];
+        int[] valuesB = new int[256];
+
+        for(char charA: a.toCharArray()){
+            valuesA[charA]++;
+        }
+        for(char charB: b.toCharArray()){
+            valuesB[charB]++;
+        }
+        for(int i = 0; i < 256; i++){
+            if(valuesA[i] != valuesB[i]) return false;
+        }
+        return true;
+
+    }
+    public static void main(String[] args){
+        String a = "listen";
+        String b = "silen<t";
+
+        System.out.println(areAnagramsArrayASCII(a, b));
+
+    }
 }

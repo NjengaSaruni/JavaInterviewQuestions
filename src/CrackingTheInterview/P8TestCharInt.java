1dbb6d051c3c56de423d46e7bd4b9f3587fca86b
Peter Njenga <peternjengask@gmail.com>
2018-11-09 16:33:56 +0300

    All characters as ints

---
+++ src/CrackingTheInterview/P8TestCharInt.java
@@ -0,0 +1,10 @@
+package CrackingTheInterview;
+
+public class P8TestCharInt {
+    public static void main(String[] args){
+        for(char i = 0; i < 256; i++){
+            System.out.printf(" Character is %c\t", i);
+            System.out.printf(" Character is %d\n\n", (int)i);
+        }
+    }
+}

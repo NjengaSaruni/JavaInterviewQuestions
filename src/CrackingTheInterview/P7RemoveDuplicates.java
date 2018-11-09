380282ce88633751aa580295774ffb982f03c14e
Peter Njenga <peternjengask@gmail.com>
2018-11-09 15:51:15 +0300

    Remove duplicates for the hub

---
+++ src/CrackingTheInterview/P7RemoveDuplicates.java
@@ -0,0 +1,12 @@
+package CrackingTheInterview;
+
+public class P7RemoveDuplicates {
+    public static char[] removeDuplicates(){
+
+    }
+
+    public static void main(String[] args){
+        char[] str = new char[] {'a', 'b', 'd', 'd', 'e', 'f'};
+        System.out.println(removeDuplicates());
+    }
+}

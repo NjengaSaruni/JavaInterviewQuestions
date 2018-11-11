6f10591258e198cce09c8e1ee201017404a818a7
Peter Njenga <peternjengask@gmail.com>
2018-11-11 12:56:49 +0300

    Recursive approach works for both even and odd numbers!

--- src/ArraysTutorial/SpiralMatrix.java
+++ src/ArraysTutorial/SpiralMatrix.java
@@ -72,8 +72,50 @@ public class SpiralMatrix {
         }
         return matrix;
     }
+
+    public static void recurse(int[][] array, int min, int max, int value){
+        int col = max;
+        int row = max;
+
+        while(col > min){
+            array[row][col] = value;
+            col--; value--;
+        }
+        while(row > min){
+            array[row][col] = value;
+            row--; value--;
+        }
+        while(col < max){
+            array[row][col] = value;
+            col++; value--;
+        }
+        while(row < max){
+            array[row][col] = value;
+            row++; value--;
+        }
+
+        if(max - min == 1){
+            return;
+        }
+        if(max == min){
+            array[min][min] = 1;
+            return;
+        }
+        recurse(array, min + 1, max - 1, value);
+    }
+
+    public static int[][] spiralRecursive(int order){
+        int[][] array = new int[order][order];
+
+        recurse(array, 0, order - 1, (int)Math.pow(order, 2));
+
+        return array;
+    }
     public static void main(String[] args){
-        for(int[] array: spiral(5)){
+//        for(int[] array: spiral(8)){
+//            System.out.println(Arrays.toString(array));
+//        1
+        for(int[] array: spiralRecursive(100)){
             System.out.println(Arrays.toString(array));
         }
     }

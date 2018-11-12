7fd98bb6524cae4e39f6087cae8932133e34171e
Peter Njenga <peternjengask@gmail.com>
2018-11-12 12:45:10 +0300

    Fast and slow runner to solve circular linked list problem

--- src/CrackingTheInterview/P9CircularLinkedList.java
+++ src/CrackingTheInterview/P9CircularLinkedList.java
@@ -4,29 +4,64 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class P9CircularLinkedList {
-    private static boolean circular(ListNode node){
+    private static ListNode circularRunner(ListNode node){
+        int fast = 0;
+        int slow = 0;
+
+        ListNode fastTemp = node;
+        ListNode slowTemp = node;
+
+        while(true){
+            fast+=2;
+            slow+=1;
+
+            slowTemp = slowTemp.next;
+            fastTemp = fastTemp.next.next;
+            if(fastTemp == null){
+                return null;
+            }
+            if(slowTemp == fastTemp){
+                System.out.printf("Fast is %d, slow is %d\n", fast, slow);
+                for(int i = 0; i < fast % slow; i++){
+                    slowTemp = slowTemp.next;
+                }
+                return slowTemp;
+            }
+        }
+    }
+    private static ListNode circular(ListNode node){
         Set<ListNode> set = new HashSet<>();
 
         while(node != null){
-            if(set.contains(node)) return true;
+            if(set.contains(node)) return node;
             set.add(node);
             node = node.next;
         }
 
-        return false;
+        return null;
     }
 
     public static void main(String[] args){
         ListNode head = new ListNode(0);
         ListNode temp = head;
         ListNode toCirculate = new ListNode(4);
-        for(int i = 0; i < 10; i++){
+        ListNode toCirculate1 = new ListNode(4);
+        for(int i = 3; i < 22; i++){
             temp.next = new ListNode(i + 1);
+            if(i == 7){
+                toCirculate = temp;
+            }
+            if(i == 13){
+                toCirculate1 = temp;
+            }
             temp = temp.next;
         }
-        temp.next = head;
+        toCirculate1.next = toCirculate;
 
-        System.out.println(circular(head));
+        ListNode answer = circularRunner(head);
+        System.out.printf("Value is %d\n", answer.value);
+        ListNode answer1 = circular(head);
+        System.out.printf("Value is %d\n", answer1.value);
     }
 }
 

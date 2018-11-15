b4c8903efef85b7f4ba2d250378669ab1e174f47
Peter Njenga <peternjengask@gmail.com>
2018-11-15 20:30:22 +0300

    Non working approach to longest palindrome

---
+++ src/LeetCode/Easy/Easy409LongestPalindrome.java
@@ -0,0 +1,33 @@
+package LeetCode.Easy;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Easy409LongestPalindrome {
+    public static String longestPalindrome(String str){
+        StringBuilder longest = new StringBuilder();
+        for(int i = 0; i < str.length(); i++){
+            int left = i;
+            int right = i + 1;
+            if(left >= 0 && right < str.length() - 1){
+                if(longest.length() < 2){
+                    if(str.charAt(left) == str.charAt(right)){
+                        longest.setLength(0);
+                        longest.append(str.charAt(left)).append(str.charAt(i));
+                    }
+                }else {
+                    if (str.charAt(left) == str.charAt(right)) {
+                        longest.setLength(0);
+                        longest.append(str.charAt(left)).append(str.charAt(i));
+                    }
+                }
+
+            }
+        }
+        System.out.println(longest);
+        return str;
+    }
+    public static void main(String[] args){
+        System.out.println(longestPalindrome("aacbcde"));
+    }
+}

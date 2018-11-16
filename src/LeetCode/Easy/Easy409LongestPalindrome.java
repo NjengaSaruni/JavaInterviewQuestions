dd66a423e3657af633b68cc893146f1e3270b7bd
Peter Njenga <peternjengask@gmail.com>
2018-11-16 10:47:35 +0300

    Algorithm approach works

--- src/LeetCode/Easy/Easy409LongestPalindrome.java
+++ src/LeetCode/Easy/Easy409LongestPalindrome.java
@@ -1,30 +1,29 @@
 package LeetCode.Easy;
 
 public class Easy409LongestPalindrome {
-    public static String longestPalindrome(String str){
-        StringBuilder longest = new StringBuilder();
-        for(int i = 0; i < str.length(); i++){
-            int left = i;
-            int right = i + 1;
-            if(left >= 0 && right < str.length() - 1){
-                if(longest.length() < 2){
-                    if(str.charAt(left) == str.charAt(right)){
-                        longest.setLength(0);
-                        longest.append(str.charAt(left)).append(str.charAt(i));
-                    }
-                }else {
-                    if (str.charAt(left) == str.charAt(right)) {
-                        longest.setLength(0);
-                        longest.append(str.charAt(left)).append(str.charAt(i));
-                    }
-                }
-
-            }
+    private static int[] palindromeIndex(String str, int left, int right){
+        if(left == 0 || right == str.length()){
+            return new int[]{left, right};
         }
-        System.out.println(longest);
+        if(str.charAt(left) == str.charAt(right)){
+            return palindromeIndex(str, left - 1, right + 1);
+        }
+        return new int[]{left, right};
+    }
+    public static String longestPalindrome(String str){
+        String longest;
+        for(int i = 0; i < str.length() - 1; i++){
+            int[] indexA = palindromeIndex(str, i, i);
+            int[] indexB = palindromeIndex(str, i, i + 1);
+
+            System.out.printf("Index A for %d is left: %d, right: %d\n", i, indexA[0], indexA[1]);
+            System.out.printf("Index B for %d is left: %d, right: %d\n", i, indexB[0], indexB[1]);
+
+        }
+
         return str;
     }
     public static void main(String[] args){
-        System.out.println(longestPalindrome("aacbcde"));
+        System.out.println(longestPalindrome("aaecbceade"));
     }
 }

fb6a3c9fec6278e50bfba0d565401b1b8753c137
Peter Njenga <peternjengask@gmail.com>
2018-11-18 10:32:06 +0300

    Accepted solution for remove Nth node from last

--- src/LeetCode/Medium/Medium19RemoveNthNode.java
+++ src/LeetCode/Medium/Medium19RemoveNthNode.java
@@ -19,15 +19,53 @@ package LeetCode.Medium;
 import Utils.ListNode;
 
 public class Medium19RemoveNthNode {
-//    public ListNode removeNthFromEnd(ListNode head, int n) {
-//
-//    }
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        int length = 1;
+        ListNode temp = head;
+        ListNode prev = null;
+
+        while(temp.next != null){
+            prev = temp;
+            temp =  temp.next;
+            length++;
+        }
+
+        n = length - n + 1;
+
+        if(length == n){
+            if(prev == null){
+                head = null;
+            }else{
+                prev.next = null;
+            }
+            return head;
+        }
+        if(n == 1){
+            return head.next;
+        }
+
+        temp = head;
+        for(int i = 1; i < n; i++){
+            prev = temp;
+            temp = temp.next;
+        }
+
+        prev.next = temp.next;
+
+        return head;
+    }
 
     public static void main(String[] args){
         ListNode node = new ListNode(2);
-        node = node.insertFront(4);
-        node.insert(9);
+        node = node.insertFront(9);
+        node = node.insertFront(8);
+        node = node.insertFront(5);
+        System.out.println(node);
+
+        node = removeNthFromEnd(node, 2);
 
         System.out.println(node);
+
+
     }
 }

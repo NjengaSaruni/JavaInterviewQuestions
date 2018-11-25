97ca57c18e11a0783498fab921d5ac4ce71876a2
Peter Njenga <peternjengask@gmail.com>
2018-11-25 23:16:05 +0300

    Search in other array

--- src/LeetCode/Medium/Medium33SearchInRotatedArray.java
+++ src/LeetCode/Medium/Medium33SearchInRotatedArray.java
@@ -19,4 +19,32 @@
 package LeetCode.Medium;
 
 public class Medium33SearchInRotatedArray {
+    public static int search(int[] nums, int target, int left, int right) {
+        if(left == right){
+            if(nums[left] == target){
+                return left;
+            }
+            return -1;
+        }
+
+        int mid = (left + right + 1) / 2;
+
+        if(nums[mid] < nums[left]){
+            if(target >= nums[left] || target < nums[mid]){
+                return search(nums, target, left, mid - 1);
+            }
+            if(target < nums[left]){
+                return search(nums, target,mid + 1, right);
+            }
+        }
+    }
+    public static int search(int[] nums, int target) {
+        return search(nums, target, 0, nums.length - 1);
+    }
+
+    public static void main(String[] args){
+        int[] array = new int[]{4,5,6,7,0,1,2};
+
+        System.out.println(search(array, 4));
+    }
 }

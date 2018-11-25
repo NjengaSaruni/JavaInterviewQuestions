ee4759ffd69def59a57c5a1b79f5feebd7ef9406
Peter Njenga <peternjengask@gmail.com>
2018-11-25 23:38:44 +0300

    Almost working solution to search in rotated array

--- src/LeetCode/Medium/Medium33SearchInRotatedArray.java
+++ src/LeetCode/Medium/Medium33SearchInRotatedArray.java
@@ -28,17 +28,16 @@ public class Medium33SearchInRotatedArray {
         }
 
         int mid = (left + right + 1) / 2;
-
-        if(nums[mid] < nums[left]){
-            if(target >= nums[left] || target < nums[mid]){
-                return search(nums, target, left, mid - 1);
-            }
-            if(target < nums[left]){
-                return search(nums, target,mid + 1, right);
-            }
+        if(nums[mid] == target){
+            return mid;
         }
 
-        return -1;
+        if(target > nums[mid]){
+            if(target >= nums[left]) return search(nums, target, left, mid - 1);
+            return search(nums, target, mid + 1, right);
+        }
+        if(target < nums[left]) return search(nums, target, mid + 1, right);
+        return search(nums, target, left, mid - 1);
     }
     public static int search(int[] nums, int target) {
         return search(nums, target, 0, nums.length - 1);
@@ -47,6 +46,6 @@ public class Medium33SearchInRotatedArray {
     public static void main(String[] args){
         int[] array = new int[]{4,5,6,7,0,1,2};
 
-        System.out.println(search(array, 4));
+        System.out.println(search(array, 0));
     }
 }

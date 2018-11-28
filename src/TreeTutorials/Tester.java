f5cfec182e93be0db7760f32121625f27dd67b5c
Peter Njenga <peternjengask@gmail.com>
2018-11-28 16:26:35 +0300

    Tester helper for BST

---
+++ src/TreeTutorials/Tester.java
@@ -0,0 +1,16 @@
+package TreeTutorials;
+
+public class Tester {
+    public static void main(String[] args){
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+
+        tree.add(1);
+        tree.add(3);
+        tree.add(2);
+        tree.add(6);
+        tree.add(7);
+        tree.add(5);
+        tree.add(9);
+
+    }
+}

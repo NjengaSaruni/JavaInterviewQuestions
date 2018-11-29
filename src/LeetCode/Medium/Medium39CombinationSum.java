3c14a5b4c1848f62479beccbcac5fae269bc6bf0
Peter Njenga <peternjengask@gmail.com>
2018-11-29 11:36:51 +0300

    Attempt at combinationSum

---
+++ src/LeetCode/Medium/Medium39CombinationSum.java
@@ -0,0 +1,14 @@
+package LeetCode.Medium;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Medium39CombinationSum {
+    public static  List<List<Integer>> combinationSum(int[] candidates, int target){
+        Set<List<Integer>> intSet = new HashSet<>();
+        Set<Integer> values = Arrays.stream(candidates).boxed().collect(Collectors.toSet());
+     }
+    public static void main(String[] args){
+
+    }
+}

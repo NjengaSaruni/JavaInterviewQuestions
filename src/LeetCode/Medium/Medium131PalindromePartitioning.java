a6f284dafe55fa50155a5285a7c1e893b7b8f7e6
Peter Njenga <peternjengask@gmail.com>
2018-11-30 11:26:05 +0300

    Fix minor bug

--- src/LeetCode/Medium/Medium131PalindromePartitioning.java
+++ src/LeetCode/Medium/Medium131PalindromePartitioning.java
@@ -22,26 +22,37 @@ public class Medium131PalindromePartitioning {
 
     public static List<List<String>> partition(String s) {
         Set<List<String>> set = new HashSet<>();
+        if(s.length() <= 1){
+            List<String> stringList = new ArrayList<>();
+            stringList.add(s);
+            set.add(stringList);
+        }
 
         for(int i = 1; i < s.length(); i++){
             String A = s.substring(0, i);
             String B = s.substring(i);
 
-            if(isPalindrome(A) && isPalindrome(B)){
-                List<String> partitions = new ArrayList<>();
-                partitions.add(A);
-                partitions.add(B);
 
+            List<String> partitions = new ArrayList<>();
+            partitions.add(A);
+            partitions.add(B);
+
+            if(isPalindrome(A) && isPalindrome(B)){
                 if(!set.contains(partitions)){
                     set.add(partitions);
                 }
+            }
 
+            if(isPalindrome(B)){
                 List<List<String>> subA = partition(A);
-                List<List<String>> subB = partition(B);
-
                 addToSet(set, B, subA);
+
+            }
+            if(isPalindrome(A)){
+                List<List<String>> subB = partition(B);
                 addToSet(set, A, subB);
             }
+
         }
 
         List<List<String>> answer = new ArrayList<>();
@@ -75,7 +86,7 @@ public class Medium131PalindromePartitioning {
         return true;
     }
     public static void main(String[] args){
-        for(List<String> list: partition("abbbb")){
+        for(List<String> list: partition("aab")){
             System.out.println(list);
         }
     }

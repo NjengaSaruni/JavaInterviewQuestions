46eccfbf7a9e9a527f5aeee70352af65ad191c23
Peter Njenga <peternjengask@gmail.com>
2018-11-30 17:39:31 +0300

    Use dictionary to memoize fibonacci in climb stairs

--- src/LeetCode/Easy/Easy70ClimbStairs.java
+++ src/LeetCode/Easy/Easy70ClimbStairs.java
@@ -8,14 +8,22 @@ public class Easy70ClimbStairs {
         if(map.containsKey(n)) {
             return map.get(n);
         }
-        int a = climbStairs(n - 1 );
-        int b = climbStairs(n - 2);
-        map.put(n , a + b);
+        int a = climbStairs(n - 1, map);
         map.put(n - 1 , a);
+        int b = climbStairs(n - 2, map);
+        map.put(n , a + b);
         map.put(n - 2, b);
         return a + b;
     }
-    public static int climbStairs(int n){
+    public static int climbStairs(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(1, 1);
+        map.put(2, 2);
+
+        return climbStairs(n, map);
+    }
+    public static int climbStairsIterative(int n){
         int a = 1;
         int b = 1;
         int i = 1;
@@ -29,6 +37,6 @@ public class Easy70ClimbStairs {
         return b;
     }
     public static void main(String[] args){
-        System.out.println(climbStairs(5));
+        System.out.println(climbStairs(38));
     }
 }

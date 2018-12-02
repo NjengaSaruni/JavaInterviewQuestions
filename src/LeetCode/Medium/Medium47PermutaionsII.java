315a36320a664ca094610083c2765adb337717d9
Peter Njenga <peternjengask@gmail.com>
2018-12-02 22:50:36 +0300

    Problem definition

--- src/LeetCode/Medium/Medium47PermutaionsII.java
+++ src/LeetCode/Medium/Medium47PermutaionsII.java
@@ -1,6 +1,22 @@
+//Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+//
+//        Example:
+//
+//        Input: [1,1,2]
+//        Output:
+//        [
+//        [1,1,2],
+//        [1,2,1],
+//        [2,1,1]
+//        ]
 package LeetCode.Medium;
 
+import java.util.List;
+
 public class Medium47PermutaionsII {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+    }
     public static void main(String[] args){
 
     }

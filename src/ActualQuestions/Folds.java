0e2e5a853fbfe4f236084e213550de814fccd944
Peter Njenga <peternjengask@gmail.com>
2018-12-03 17:39:16 +0300

    Working recursive solution to folds problem

---
+++ src/ActualQuestions/Folds.java
@@ -0,0 +1,30 @@
+package ActualQuestions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Folds {
+    public static List<Boolean> foldsRecursive(int n){
+        List<Boolean> list = new ArrayList<>();
+
+        if(n == 1){
+            list.add(false);
+            return list;
+        }
+
+        List<Boolean> former = foldsRecursive(n - 1);
+        list.addAll(former);
+        former.add(false);
+        int count = former.size() - 1;
+
+        while (count >= 0){
+            list.add(!former.get(count));
+            count--;
+        }
+
+        return list;
+    }
+    public static void main(String[] args){
+        System.out.println(foldsRecursive(3));
+    }
+}
